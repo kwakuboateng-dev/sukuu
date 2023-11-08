a21c82655454bf80a663e30bd093150d8696cf34
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-08 17:01:23 +0000

    project: created server, created test route, created unknown routes middleware

---
+++ server/app.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config'; // This is the same as import dotenv from 'dotenv'; dotenv.config();
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+export const app = express();
+
+
+// Body parsing Middleware
+app.use(express.json({ limit: '50mb' }));
+
+// Cookie parsing Middleware
+app.use(cookieParser());
+
+// Cors Middleware
+app.use(cors(
+  {
+    origin: process.env.ORIGIN || 'http://localhost:3000'
+  }
+));
+
+
+// Test route
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: 'API is working!'
+  });
+});
+
+// Unknown routes Middleware
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
