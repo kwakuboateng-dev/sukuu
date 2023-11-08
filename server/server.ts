86736493c4d8ac0d52febf4c141d62b3b42d01cb
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-08 17:19:34 +0000

    project: MongoDB and Redis setup

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app';
 import 'dotenv/config';
+import connectDB from './utils/db';
 
 
 //create server
 app.listen(process.env.PORT || 8800, () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  connectDB();
 });
