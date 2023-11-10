833e06df0646148e449b6bfdd906aff2be01ee02
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-10 15:12:43 +0000

    role authorization middleware

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { activateUser, loginUser, logoutUser, registerUser } from '../controllers/user.controller';
-import { isAuthenticated } from '../middleware/auth';
+import { authorizeRoles, isAuthenticated } from '../middleware/auth';
 
 const userRouter = express.Router();
 
@@ -10,7 +10,7 @@ userRouter.post('/activate-user', activateUser);
 
 userRouter.post('/login', loginUser);
 
-userRouter.get('/logout', isAuthenticated, logoutUser);
+userRouter.get('/logout', isAuthenticated, authorizeRoles('admin'), logoutUser);
 
 
 export default userRouter;
